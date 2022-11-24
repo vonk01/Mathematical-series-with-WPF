6556d9ff6b24e92b8dd438e8f571aff29e5c35f1
vonk01 <vonk01@icloud.com>
2022-11-24 22:04:43 +0400

    Добавьте файлы проекта.

---
+++ IndividualWPF22.11.18.3.2/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace IndividualWPF22._11._18._3._2
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ IndividualWPF22.11.18.3.2/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IndividualWPF22._11._18._3._2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private double SeriesCalc(double x, double n, double k)
+        {
+            double z = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < k; j++)
+                {
+                    z = (Math.Cos(Math.Pow(x, i)) + j) / (i * j);
+                }
+            }
+            return z;
+           
+        }
+
+        private void ButtonCalc_Click(object sender, RoutedEventArgs e)
+        {
+            double x = Convert.ToDouble(PerX.Text);
+            double n = Convert.ToDouble(PerN.Text);
+            double k = Convert.ToDouble(PerK.Text);
+            MessageBox.Show("Ответ: " + SeriesCalc(x, n, k).ToString("F"));
+        }
+    }
+}
